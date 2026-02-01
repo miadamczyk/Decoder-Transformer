672a416db85bfd44f0ff7cfa96959fbba7433603
miadamczyk <133974024+miadamczyk@users.noreply.github.com>
2026-02-01 15:38:16 +0100

    Delete scripts.sh

--- scripts.sh
+++
@@ -1,3 +0,0 @@
-python main.py --attn_type custom --d_model 512 --n_heads 8 --n_layers 6 --block_size 128 --wandb
-
-python generate.py --model_dir "models" --model_name "best_model.pt" --start_text "ROMEO: " --max_new_tokens 1000 --device cuda
